34f397e1789f2ee6aa03d6c45625a75d6a502082
zalof <zalof299@gmail.com>
2024-06-18 18:22:51 +0300

    some visual changes

--- widgets/level/script.js
+++ widgets/level/script.js
@@ -19,9 +19,9 @@ document.addEventListener("DOMContentLoaded", function () {
     document.getElementById('lvl').innerText = "LVL"+level;
 
     const progress1 = /^\d+,\d+$/.test(urlParams.get('p1')) ? urlParams.get('p1').split(',').map(Number) : [0, 100];
-    document.getElementById('p1').style.width = `${progress1[0] / progress1[1] * 100}%`;
+    document.getElementById('p1').style.width = `${Math.max(progress1[0] / progress1[1] * 100, 6)}%`;
 
     const progress2 = /^\d+,\d+$/.test(urlParams.get('p2')) ? urlParams.get('p2').split(',').map(Number) : [0, 100];
-    document.getElementById('p2').style.width = `${progress2[0] / progress2[1] * 100}%`;
+    document.getElementById('p2').style.width = `${Math.max(progress2[0] / progress2[1] * 100, 6)}%`;
 
 });
